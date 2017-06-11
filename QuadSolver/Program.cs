84eeb0e51d258b436a9c58b5365de0d08f8ad00e
shaybanerjee <shayon@live.ca>
2017-06-10 23:59:39 -0400

    first attempt

---
+++ QuadSolver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuadSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wReadValue = "";
+            while (true)
+            {
+                Console.WriteLine("Enter Expression or type 'Done' to quit: ");
+                wReadValue = Console.ReadLine();
+                if (wReadValue == "Done")
+                    break;
+                Solver.SolveExp(wReadValue);
+            }
+            
+        }
+    }
+}
