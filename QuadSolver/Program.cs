a798345694a89460b7cb30dda47be9b9c0f74753
shaybanerjee <shayon@live.ca>
2017-06-11 00:10:15 -0400

    Added to Program.cc

--- QuadSolver/Program.cs
+++ QuadSolver/Program.cs
@@ -10,6 +10,7 @@ namespace QuadSolver
             while (true)
             {
                 Console.WriteLine("Enter Expression or type 'Done' to quit: ");
+                Console.WriteLine("Expression in the form: ax^2+bx+c:");
                 wReadValue = Console.ReadLine();
                 if (wReadValue == "Done")
                     break;
